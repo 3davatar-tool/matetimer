bfcc83ab4afec6784ab24a3b282b53404794af45
3davatar-tool <webmanager@kokokonomi.co.jp>
2026-03-03 17:51:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'matetimer-v15-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './timer_v15_ios_optimized.html',
+    './manifest.json',
+    './backimage/back_image.png',
+    './sounds/finish.mp3',
+    './avatar/welcomecat.vrm',
+    './motions/VRMA_01.vrma',
+    './motions/VRMA_02.vrma',
+    './motions/VRMA_03.vrma',
+    './motions/VRMA_04.vrma',
+    './motions/VRMA_05.vrma',
+    './motions/VRMA_06.vrma',
+    './motions/VRMA_07.vrma'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache and adding assets');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// ネットワーク優先、失敗したらキャッシュを返す
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
